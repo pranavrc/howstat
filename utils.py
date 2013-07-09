805411bdd382bb22281b7abafc8d9c338539cf5f
Pranav Ravichandran <me@onloop.net>
2013-07-09 08:14:52 +0530

    URL parameter mappings.

--- utils.py
+++ utils.py
@@ -7,6 +7,47 @@ from lxml.html.clean import clean_html
 foo = lh.parse('http://stats.espncricinfo.com/stats/engine/player/45789.html?class=11;type=allround;template=results')
 foo = foo.xpath("//tr[contains(@class, 'data1')]")
 
+class Mappings:
+    self.country_codes = {
+        'Afghanistan' : 40,
+        'Australia' : 2,
+        'Bangladesh' : 25,
+        'Bermuda' : 12,
+        'England' : 1,
+        'Hong Kong' : 19,
+        'India' : 6,
+        'Ireland' : 29,
+        'Netherlands' : 15,
+        'New Zealand' : 5,
+        'Pakistan' : 7,
+        'Scotland' : 30,
+        'South Africa' : 3,
+        'Sri Lanka' : 8,
+        'West Indies' : 4,
+        'Zimbabwe' : 9
+    }
+
+    self.venues = {
+        'home' : 1,
+        'away' : 2,
+        'neutral' : 3
+    }
+
+    self.formats = {
+        'Tests' : 1,
+        'ODIs' : 2,
+        'T20Is' : 3,
+        'All' : 11
+    }
+
+    self.mappings = {
+        'class' : self.formats,
+        'opposition' : self.country_codes,
+        'host' : self.country_codes,
+        'home_or_away' : self.venues,
+        'year' : ''
+    }
+
 class PlayerFinder:
     base_url = "http://stats.espncricinfo.com"
     def __init__(self, player_name):
